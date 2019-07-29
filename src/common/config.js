4a2bb09b53a88b6dc6ba1ac31893c8cecc711a8d
Kevin Lamping <klamping@gmail.com>
2019-07-29 10:30:40 -0500

    update api endpoint

--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,4 @@
 // export const API_URL = "http://localhost:3000/api"
 // export const API_URL = "https://conduit.productionready.io/api"
-export const API_URL = "http://167.71.137.67:3000/api";
+export const API_URL = "https://conduit-api.learnwebdriverio.com/api";
 export default API_URL;
