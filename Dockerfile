a7293d218612652cbae7be7c783db4b0597cc63d
Kevin Lamping <klamping@gmail.com>
2020-05-02 21:22:03 -0500

    add Docker information; switch to using local api server by default

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:latest
+WORKDIR /app
+COPY . /app/
+RUN yarn install
+EXPOSE 8080
+CMD [ "yarn", "serve" ]
--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,23 @@
-// export const API_URL = "http://localhost:3000/api"
-// export const API_URL = "https://conduit.productionready.io/api"
-export const API_URL = "https://conduit-api.learnwebdriverio.com/api";
+const API_ENDPOINTS = {
+  local: "http://localhost:3000/api",
+  official: "https://conduit.productionready.io/api",
+  demo: "https://conduit-api.learnwebdriverio.com/api"
+};
+
+let apiEndpoint = process.env.API_ENDPOINT;
+
+// make sure proper endpoint option passed in
+if (!API_ENDPOINTS.hasOwnProperty(apiEndpoint)) {
+  if (apiEndpoint !== undefined) {
+    throw new Error(
+      `No endpoint found for "${apiEndpoint}". Available options: ${Object.keys(
+        API_ENDPOINTS
+      )}`
+    );
+  } else {
+    apiEndpoint = "local";
+  }
+}
+
+export const API_URL = API_ENDPOINTS[apiEndpoint];
 export default API_URL;
